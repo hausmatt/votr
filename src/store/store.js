eee607acd871dd56f02813783cc11d55a6de49b5
Michael Stadelmann <sti@zuehlke.com>
2018-06-19 14:55:54 +0200

    add test item to initial state

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import * as actionTypes from './actions'
-import Auth from '../service/auth'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as actionTypes from './actions';
+import Auth from '../service/auth';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -12,27 +12,37 @@ export default new Vuex.Store({
       user: undefined
     },
     voting: {
-      id: undefined,
-      items: []
+      id: 1,
+      items: [{
+        id: 123,
+        title: 'First Item',
+        description: 'This is an item! Yay! 42!',
+        ratings: [5, 4, 4.5],
+        comments: [{
+          id: 101010,
+          text: 'This is so awesome!',
+          timestamp: Date.now()
+        }]
+      }]
     }
   },
   mutations: {
     [actionTypes.LOGIN_SUCCESSFUL] (state, payload) {
-      state.auth.loggedIn = true
-      state.auth.user = {...payload.user}
+      state.auth.loggedIn = true;
+      state.auth.user = {...payload.user};
     },
     [actionTypes.LOGIN_FAILURE] (state) {
-      state.auth.loggedIn = false
+      state.auth.loggedIn = false;
     },
     [actionTypes.LOGOUT] (state) {
-      state.auth.loggedIn = false
+      state.auth.loggedIn = false;
     }
   },
   actions: {
     [actionTypes.LOGIN_WITH_GOOGLE] ({commit}) {
       Auth.loginWithGoogle()
         .then((login) => {
-          console.log(login)
+          console.log(login);
           return commit({
             type: actionTypes.LOGIN_SUCCESSFUL,
             user: {
@@ -41,26 +51,26 @@ export default new Vuex.Store({
               email: login.user.email,
               photoUrl: login.user.photoUrl
             }
-          })
+          });
         })
-        .catch(() => commit(actionTypes.LOGIN_FAILURE))
+        .catch(() => commit(actionTypes.LOGIN_FAILURE));
     },
     [actionTypes.LOGOUT] ({commit}) {
       Auth.logout()
         .then(() => commit(actionTypes.LOGOUT))
-        .catch(() => commit(actionTypes.LOGOUT))
+        .catch(() => commit(actionTypes.LOGOUT));
     }
   },
   getters: {
     isUserLoggedIn: (state) => () => {
-      return state.auth.loggedIn
+      return state.auth.loggedIn;
     },
     userDisplayName: (state) => () => {
       if (state.auth.user) {
-        return state.auth.user.displayName
+        return state.auth.user.displayName;
       } else {
-        return undefined
+        return undefined;
       }
     }
   }
-})
+});

38fa1311f44408e304ad9860a13dadee2c9fb930
mhau <matthias.hauser@zuehlke.com>
2018-06-21 09:59:38 +0200

    firebase deploy stuff

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 export const firebaseConfig = {
-    apiKey: 'AIzaSyA4-WjLkeheCzFN8XuP350Gpr0IhNCnojg',
-    authDomain: 'ze-votr.firebaseapp.com',
-    databaseURL: 'https://ze-votr.firebaseio.com',
-    projectId: 'ze-votr',
-    storageBucket: 'ze-votr.appspot.com',
-    messagingSenderId: '117346976017'
+    apiKey: "AIzaSyA4-WjLkeheCzFN8XuP350Gpr0IhNCnojg",
+    authDomain: "ze-votr.firebaseapp.com",
+    databaseURL: "https://ze-votr.firebaseio.com",
+    projectId: "ze-votr",
+    storageBucket: "ze-votr.appspot.com",
+    messagingSenderId: "117346976017"
 };
 
 export function initFirebase() {
